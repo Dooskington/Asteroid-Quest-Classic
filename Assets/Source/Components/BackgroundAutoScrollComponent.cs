e698c2752cf6cf96e7ab63b3aa9c9f38d85b51a6
Declan Hopkins <hopkins.declan@gmail.com>
2017-01-13 13:44:38 -0500

    Implement primitive main menu

---
+++ Assets/Source/Components/BackgroundAutoScrollComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAutoScrollComponent : MonoBehaviour
+{
+    public GameObject[] backgrounds;
+    public float[] speeds;
+
+    private void Update()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            GameObject bg = backgrounds[i];
+            float speed = speeds[i];
+
+            bg.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", 
+                new Vector2((Time.time / 4.0f) * speed, (Time.time / 4.0f) * speed));
+        }
+    }
+}
---
+++ Assets/Source/Components/UI/UIQuitButtonComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQuitButtonComponent : MonoBehaviour
+{
+    public void OnQuitClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
