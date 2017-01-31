c81782debb548fefcbe1dd07658f3a4c4561ce98
Declan Hopkins <hopkins.declan@gmail.com>
2017-01-31 09:59:43 -0500

    Add some station music

---
+++ Assets/Source/Components/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource backgroundMusic;
+    public AudioSource stationMusic;
+    public GameObject stationPanel;
+
+    public void Update()
+    {
+        if (stationPanel.activeSelf)
+        {
+            backgroundMusic.volume = 0.0f;
+
+            if (!stationMusic.isPlaying)
+            {
+                stationMusic.Play();
+            }
+        }
+        else
+        {
+            backgroundMusic.volume = 0.5f;
+
+            if (stationMusic.isPlaying)
+            {
+                stationMusic.Stop();
+            }
+        }
+    }
+}
